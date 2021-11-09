76286ea0c07372f63b65aa6dd83e7c4defb0be9b
k.marczynski <k.marczynski@ocado.com>
2021-11-09 16:55:48 +0100

    add more types

--- code/preprocessor.py
+++ code/preprocessor.py
@@ -157,6 +157,7 @@ number_parses = [
     'YearsCoding',
     'YearsCodingProf',
     'YearsProgram',
+    'SOFindAnswer'
 ]
 
 agree_keys = [
@@ -238,6 +239,7 @@ important_keys = [
     'ImportantHiringRep',
     'ImportantHiringTechExp',
     'ImportantHiringTitles',
+    'AssessJobIndustry',
 ]
 
 satisfied_keys = [
@@ -254,6 +256,15 @@ satisfied_keys = [
 influence_keys = [
     'InfluenceWorkstation',
     'InfluenceHardware',
+    'InfluenceServers',
+    'InfluenceTechStack',
+    'InfluenceDeptTech',
+    'InfluenceVizTools',
+    'InfluenceDatabase',
+    'InfluenceCloud',
+    'InfluenceConsultants',
+    'InfluenceRecruitment',
+    'InfluenceCommunication'
 ]
 
 yes_no_keys = [
@@ -271,6 +282,8 @@ yes_no_keys = [
     'FizzBuzz',
     'BetterLife',
     'OffOn',
+    'EthicalImplications',
+    'Trans'
 ]
 
 last_three_months_keys = [
@@ -281,6 +294,10 @@ last_three_months_keys = [
     'StackOverflowNewQuestion',
     'StackOverflowAnswer',
     'StackOverflowMetaChat',
+    'StackOverflowFoundAnswer',
+    'StackOverflowVisit',
+    'StackOverflowParticipate',
+    'SOVisitFreq',
 ]
 
 last_three_months_strs = {
@@ -295,13 +312,13 @@ yes_no_strs = {
     'yes': 1,
     'no': 0,
     # todo replace "2" with nan/None
-    "i'm not sure/i don't know": 2,
-    "i'm not sure/i can't remember": 2,
-    "i'm not sure": 2,
-    "i'm not sure / i can't remember": 2,
-    'not sure': 2,
-    'what?': 2,
-    "not sure / can't remember": 2
+    "i'm not sure/i don't know": 666,
+    "i'm not sure/i can't remember": 666,
+    "i'm not sure": 666,
+    "i'm not sure / i can't remember": 666,
+    'not sure': 666,
+    'what?': 666,
+    "not sure / can't remember": 666
 }
 
 influence_strs = {
@@ -495,7 +512,192 @@ replacers = [
     (last_three_months_keys, last_three_months_strs),
     (important_keys, important_strs),
     (yes_no_keys, yes_no_strs),
-    (['Gender'], gender_strs)
+    (['Gender'], gender_strs),
+    (['ProgramHobby'], {
+        "yes, both": 3,
+        "yes, i program as a hobby": 2,
+        "yes, i contribute to open source projects": 1,
+        "no": 0,
+    }),
+    (['University', 'Student'], {
+        "yes, full-time": 2,
+        "yes, part-time": 1,
+        "no": 0,
+    }),
+    (['EmploymentStatus', 'Employment'], {
+        "not employed, and not looking for work": 0,
+        "not employed, but looking for work": 1,
+        "independent contractor, freelancer, or self-employed": 2,
+        "retired": 3,
+        "employed part-time": 4,
+        "employed full-time": 5,
+    }),
+    (['FormalEducation', 'HighestEducationParents', 'EducationParents', 'EdLevel'], {
+        "doctoral degree": 8,
+        'a doctoral degree': 8,
+        "master's degree": 7,
+        "a master's degree": 7,
+        "bachelor's degree": 6,
+        "a bachelor's degree": 6,
+        "some college/university study without earning a bachelor's degree": 5,
+        "some college/university study, no bachelor's degree": 5,
+        "professional degree": 4,
+        'a professional degree': 4,
+        'high school': 2,
+        "secondary school": 2,
+        "primary/elementary school": 1,
+        "i never completed any formal education": 0,
+        'no education': 0,
+        "bachelor’s degree (ba, bs, b.eng., etc.)": 11,
+        "associate degree": 12,
+        "some college/university study without earning a degree": 13,
+        "master’s degree (ma, ms, m.eng., mba, etc.)": 14,
+        "secondary school (e.g. american high school, german realschule or gymnasium, etc.)": 15,
+        "professional degree (jd, md, etc.)": 17,
+        "other doctoral degree (ph.d, ed.d., etc.)": 19,
+        "they never completed any formal education": 26,
+    }),
+    (['HomeRemote', 'WorkRemote'], {
+        "all or almost all the time (i'm full-time remote)": 6,
+        "more than half, but not all, the time": 5,
+        "about half the time": 4,
+        "less than half the time, but at least one day each week": 3,
+        "a few days each month": 2,
+        "it's complicated": 1,
+        "never": 0,
+        "less than once per month / never": 0,
+
+    }),
+    (['JobSeekingStatus', 'JobSearchStatus', 'JobSeek'], {
+        "i\'m not actively looking, but i am open to new opportunities": 1,
+        "i am actively looking for a job": 2,
+        "i am not interested in new job opportunities": 3,
+    }),
+    (['LastNewJob', 'LastHireDate'], {
+        "not applicable/ never": 1,
+        "between 2 and 4 years ago": 2,
+        "more than 4 years ago": 3,
+        "less than a year ago": 4,
+        "between 1 and 2 years ago": 5,
+        "i\'ve never had a job": 6,
+
+        "1-2 years ago": 7,
+
+        "nA - i am an independent contractor or self employed": 8,
+        "3-4 years ago": 7,
+
+    }),
+    (['TimeFullyProductive'], {
+        "one to three months": 1,
+        "three to six months": 2,
+        "less than a month": 3,
+        "six to nine months": 4,
+        "more than a year": 5,
+        "nine months to a year": 6,
+    }),
+    (['EthicsChoice'], {
+        "no": 0,
+        "depends on what it is": 1,
+        "yes": 2,
+    }),
+    (['EthicsReport'], {
+        "yes, and publicly": 1,
+        "depends on what it is": 2,
+
+        "yes, but only within the company": 3,
+        "no": 4,
+    }),
+    (['HoursOutside'], {
+        "1 - 2 hours": 1,
+        "30 - 59 minutes": 2,
+        "less than 30 minutes": 3,
+        "3 - 4 hours": 4,
+        "over 4 hours": 5
+    }),
+    (['SkipMeals', 'Exercise'], {
+        "never": 1,
+        "3 - 4 times per week": 2,
+        "1 - 2 times per week": 3,
+        "daily or almost every day": 4,
+        "i don\'t typically exercise": 5,
+    }),
+    (['OpenSourcer'], {
+        "never": 0,
+        "less than once per year": 1,
+        "once a month or more often": 2,
+        "less than once a month but more than once per year": 3
+    }),
+    (['OpenSourceQuality'], {
+        "the quality of oSS and closed source software is about the same": 0,
+        "oSS is, on average, of hIGHER quality than proprietary / closed source software": 1,
+        "oSS is, on average, of lOWER quality than proprietary / closed source software": 2,
+    }),
+    (['MgrIdiot'], {
+        "not at all confident": 0,
+        "very confident": 1,
+        "somewhat confident": 2,
+        "i don\'t have a manager": 3,
+    }),
+    (['MgrWant'], {
+        "not sure": 0,
+        "no": 1,
+        "yes": 2,
+        "i am already a manager": 3,
+    }),
+    (['ImpSyn'], {
+        "average": 0,
+        "a little below average": 1,
+        "a little above average": 2,
+        'far above average': 3,
+        'far below average': 4,
+    }),
+    (['CodeRev'], {
+        "no": 0,
+        "yes, because i see value in code review": 0,
+        "yes, because i was told to do so": 0,
+    }),
+    (['UnitTests'], {
+        "no, but i think we should": 0,
+        "yes, it\'s part of our process": 1,
+        "yes, it\'s not part of our process but the developers do it on their own": 2,
+        "no, and i\'m glad we don\'t": 3,
+    }),
+    (['PurchaseWhat'], {
+        "i have little or no influence": 0,
+        "i have some influence": 1,
+        "i have a great deal of influence": 2,
+    }),
+    (['Extraversion'], {
+        "in real life (in person)": 0,
+        "neither": 1,
+        "online": 2,
+    }),
+
+    (['SOTimeSaved'], {
+        "stack overflow was much faster": 0,
+        "they were about the same": 1,
+        "stack overflow was slightly faster": 2,
+        "the other resource was slightly faster": 3,
+        "the other resource was much faster": 4,
+    }),
+    (['SOPartFreq'], {
+        "a few times per month or weekly": 0,
+        "less than once per month or monthly": 1,
+        "i have never participated in q&a on stack overflow": 2,
+        "a few times per week": 3,
+        "daily or almost daily": 4,
+        "multiple times per day": 5,
+    }),
+    (['SOComm'], {
+        "neutral": 0,
+        "yes, somewhat": 1,
+        "no, not really": 2,
+        "yes, definitely": 3,
+        "no, not at all": 4,
+        "not sure": 5,
+    }
+     ),
+    (['WorkStart'], {})
 ]
 
 hdi = pd.read_csv(f'HDI.csv', index_col="Country")
